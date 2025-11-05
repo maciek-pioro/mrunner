26f68fd2824fe1bad59828ae389a6c7059dc8ce5
Maciej Pióro <m@ciekpio.ro>
2025-11-05 13:57:50 +0100

    Up the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mrunner",
-    version="24.11",
+    version="25.11",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
